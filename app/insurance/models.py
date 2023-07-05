004246dc445eb0c5b56ca4f085273354af418eae
Михаил <mxoilslukin@gmail.com>
2023-07-05 14:09:44 +0500

    final

--- app/insurance/models.py
+++ app/insurance/models.py
@@ -1,20 +1,7 @@
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
-
-
-class Rates(models.Model):
-    """Модель тарифов."""
-    id = fields.IntField(pk=True)
-    type = fields.TextField(null=False)
-    price = fields.FloatField(null=False)
-    days = fields.IntField(null=False)
-    period = fields.IntField(null=False)
-    discount_amount = fields.FloatField(null=True)
-
-
-Rate_in_Pydantic = pydantic_model_creator(
-    Rates, name="Rate In", exclude_readonly=True)
-Rates_Pydantic = pydantic_model_creator(Rates, name="Rate")
+from pydantic import BaseModel
+from typing import Dict, List
 
 
 class Material(models.Model):
@@ -29,13 +16,15 @@ Material_in_Pydantic = pydantic_model_creator(
 Material_Pydantic = pydantic_model_creator(Material, name="Material")
 
 
-class Order(models.Model):
-    """Модель вычисления стоимости страховки."""
-    id = fields.IntField(pk=True)
-    material = fields.TextField()
-    rate = fields.TextField()
-    count = fields.IntField()
+class Rate(BaseModel):
+    cargo_type: str
+    rate: str
 
 
-Order_in_Pydantic = pydantic_model_creator(
-    Order, name="Order In", exclude_readonly=True)
+class RateData(BaseModel):
+    date: str
+    tariffs: List[Rate]
+
+
+class RequestData(BaseModel):
+    data: Dict[str, List[Rate]]
--- app/insurance/router.py
+++ app/insurance/router.py
@@ -1,29 +1,13 @@
 from fastapi import APIRouter
 from typing import List
 
-from .models import (Rates, Material,
-                     Rates_Pydantic, Rate_in_Pydantic,
-                     Material_in_Pydantic, Material_Pydantic,
-                     Order_in_Pydantic)
+from .models import Material, Material_in_Pydantic, Material_Pydantic, RequestData
 
 
 router = APIRouter(
     prefix="/insurance",
     tags=["Insurance"]
 )
-
-
-@router.get('/ratesList', response_model=List[Rates_Pydantic])
-async def get_rates():
-    """Вывод всех тарифов."""
-    return await Rates_Pydantic.from_queryset(Rates.all())
-
-
-@router.post("/createRate", response_model=Rates_Pydantic)
-async def create_rate(rate: Rate_in_Pydantic):
-    """Добавление тарифа."""
-    user_obj = await Rates.create(**rate.dict(exclude_unset=True))
-    return await Rates_Pydantic.from_tortoise_orm(user_obj)
 
 
 @router.get('/material', response_model=List[Material_Pydantic])
@@ -39,14 +23,18 @@ async def create_material(material: Material_in_Pydantic):
     return await Material_Pydantic.from_tortoise_orm(user_obj)
 
 
-@router.post("/createOrder")
-async def create_order(order: Order_in_Pydantic):
-    """Вычисление стоимости страховки."""
-    material = order.dict(exclude_unset=True)["material"]
-    rate = order.dict(exclude_unset=True)["rate"]
-    count = order.dict(exclude_unset=True)["count"]
-    print(count)
-    matPrice = await Material.filter(type=material).values('price')
-    ratePrice = await Rates.filter(type=rate).values('price')
-    resPrice = int(ratePrice[0]['price']) * int(matPrice[0]['price']) * count
-    return {"Цена страхования": resPrice}
+@router.post("/calculate_insurance_cost")
+async def calculate_insurance_cost(request_data: RequestData):
+    """Вычисление стоимости заказа"""
+    result = {}
+    for date, rates in request_data.data.items():
+        result[date] = []
+        for rate in rates:
+            cargo_type = rate.cargo_type
+            rate = rate.rate
+            cargoPrice = await Material.filter(type=cargo_type).values("price")
+            print(cargoPrice)
+            result[date].append({"cargo_price": float(
+                cargoPrice[0]['price']) * float(rate)})
+            print(result)
+    return result
